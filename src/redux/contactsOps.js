9c11e6b1ab13e7f79515c9f25da88850400a8b45
Dikens27 <kar.karovich321@gmail.com>
2024-03-29 15:06:12 +0200

    fixed

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -5,36 +5,36 @@ axios.defaults.baseURL = 'https://65ffeddddf565f1a61458105.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
-  async (_, thankAPI) => {
+  async (_, thunkAPI) => {
     try {
       const response = await axios.get('/contacts');
       return response.data;
     } catch (error) {
-      return thankAPI.rejectWithValue(error.massage);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async (newContact, thankAPI) => {
+  async (newContact, thunkAPI) => {
     try {
       const response = await axios.post('/contacts', newContact);
       return response.data;
     } catch (error) {
-      return thankAPI.rejectWithValue(error.massage);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
-  async (contactId, thankAPI) => {
+  async (contactId, thunkAPI) => {
     try {
       const response = await axios.delete(`/contacts/${contactId}`);
       return response.data;
     } catch (error) {
-      return thankAPI.rejectWithValue(error.massage);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
